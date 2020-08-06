ec815fd31b506a4f75c44a95a3766ce0cda44d76
behran <agolovev@gismeteo.ru>
2020-08-06 18:33:24 +0300

    Refactoring store

--- src/internal/app/model/testing.go
+++ src/internal/app/model/testing.go
@@ -5,7 +5,7 @@ import "testing"
 // TestUser return user
 func TestUser(t *testing.T) *User {
 	return &User{
-		Email:    "user-2@exmaple.org",
+		Email:    "user-4@exmaple.org",
 		Password: "PasswordHard",
 	}
 }
